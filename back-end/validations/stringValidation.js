48d1f7f96f4db42ce4e5754a4be9efabf285da68
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-02-11 20:30:22 -0500

    refactored stringValidation.js

--- back-end/validations/stringValidation.js
+++ back-end/validations/stringValidation.js
@@ -1,11 +1,3 @@
-// Expected: "Spiders on a Log"
-// Received: "spiders on a log"
-
-// Expected: "CoBbos"
-// Received: "COMBOS"
-
-// Expected: "Flamin' Hot Cheetoes"
-// Received: "FLAMIN' hot Cheetoes"
 
 const validateSpace = (snack) => {
     let array = snack.name.split(" ");
@@ -22,29 +14,4 @@ const validateSpace = (snack) => {
     return upperFunc
 }
 
-
-
 module.exports = validateSpace;
-
-
-
-
-
-
-
-
-
-
-
-// let testing = snack.name.split(" ")
-// console.log("THE SPLIT",testing)
-// let mapFunction = testing.map((el)=> {
-//     if(el.length > 2){
-//         console.log(el.charAt(0).toUpperCase() + el.slice(1).toLowerCase())
-//         el.charAt(0).toUpperCase() + el.slice(1).toLowerCase()
-//     }else{
-//         console.log(el.toLowerCase());
-//         el.toLowerCase();
-//     }
-// })
-// console.log(mapFunction)
